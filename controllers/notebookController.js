c674184e594abfca165e8a0a0df4824ebf849a10
benjoboy <serec.benjamin@gmail.com>
2020-10-15 17:23:24 +0200

    Notebook/note routes complete

--- controllers/notebookController.js
+++ controllers/notebookController.js
@@ -8,42 +8,177 @@ ObjectId = require("mongodb").ObjectID;
  * @description :: Server-side logic for managing notebooks.
  */
 module.exports = {
-  create: function (req, res) {
-    console.log(req.session.userId);
-    var notebook = new notebookModel({
-      userId: ObjectId(req.session.userId),
-      date: new Date(),
-      title: req.body.title,
-    });
-
-    notebook.save(function (err, notebook) {
-      if (err) {
-        return res.status(500).json({
-          message: "Error when creating notebook",
-          error: err,
-        });
+  list: function (req, res) {
+    notebookModel.find(
+      {
+        userId: ObjectId(req.session.userId),
+      },
+      function (err, list) {
+        if (err) {
+          return res.status(500).json({
+            message: "Error getting notebooks",
+            error: err,
+          });
+        }
+        return res.status(200).json(list);
       }
-      return res.status(201).json(notebook);
-    });
+    );
+  },
+
+  create: function (req, res) {
+    if (req.session.userId) {
+      var notebook = new notebookModel({
+        userId: ObjectId(req.session.userId),
+        date: new Date(),
+        title: req.body.title,
+      });
+
+      notebook.save(function (err, notebook) {
+        if (err) {
+          return res.status(500).json({
+            message: "Error when creating notebook",
+            error: err,
+          });
+        }
+        return res.status(201).json({
+          status: "created",
+        });
+      });
+    } else
+      res.status(500).json({
+        message: "user not logged in",
+      });
   },
 
   addNote: function (req, res) {
-    //TODO addNote
-    return null;
+    notebookModel.updateOne(
+      {
+        _id: ObjectId(req.body.id),
+        userId: ObjectId(req.session.userId), //authentication
+      },
+      {
+        $push: {
+          notes: {
+            title: req.body.title,
+            content: req.body.content,
+            editDate: new Date(),
+          },
+        },
+      },
+      function (err, note) {
+        if (err) {
+          return res.status(500).json({
+            message: "Error adding a note",
+            error: err,
+          });
+        } else if (note.nModified === 0) {
+          return res.status(500).json({
+            message: "Error adding a note, user probably not loged in",
+          });
+        }
+        return res.status(201).json({
+          status: "created",
+        });
+      }
+    );
   },
 
   update: function (req, res) {
-    //TODO
-    return null;
+    console.log(req.params.id);
+    if (!req.body.title) {
+      return res.status(500).json({
+        message: "No title",
+      });
+    }
+    notebookModel.updateOne(
+      {
+        _id: ObjectId(req.params.id),
+        userId: ObjectId(req.session.userId), //auth
+      },
+      {
+        $set: {
+          title: req.body.title,
+        },
+      },
+      function (err, notebook) {
+        if (err) {
+          return res.status(500).json({
+            message: "Error updating notebook",
+            error: err,
+          });
+        } else if (notebook.nModified === 0) {
+          console.log(notebook);
+          return res.status(500).json({
+            message: "Error updating notebook, user probably not loged in",
+          });
+        }
+        return res.status(204).json({});
+      }
+    );
+  },
+  updateNote: function (req, res) {
+    notebookModel.updateOne(
+      {
+        userId: ObjectId(req.session.userId),
+        _id: ObjectId(req.params.notebookId),
+        "notes._id": ObjectId(req.params.noteId),
+      },
+      {
+        $set: {
+          "notes.$.title": req.body.title,
+          "notes.$.content": req.body.content,
+          "notes.$.editDate": new Date(),
+        },
+      },
+      function (err, note) {
+        if (err) {
+          return res.status(500).json({
+            message: "Error updating a note",
+            error: err,
+          });
+        }
+        return res.status(200).json({
+          status: "updated",
+          message: "note was updated",
+        });
+      }
+    );
   },
 
   remove: function (req, res) {
-    //TODO
+    notebookModel.deleteOne(
+      { _id: ObjectId(req.params.id), userId: ObjectId(req.session.userId) },
+      function (err, notebook) {
+        if (err) {
+          return res.status(500).json({
+            message: "Error deleting notebook",
+            error: err,
+          });
+        }
+        return res.status(204).json();
+      }
+    );
     return null;
   },
 
   removeNote: function (req, res) {
-    //TODO
-    return null;
+    notebookModel.updateOne(
+      {
+        userId: ObjectId(req.session.userId),
+        _id: ObjectId(req.params.notebookId),
+      },
+      {
+        $pull: { notes: { _id: ObjectId(req.params.noteId) } },
+      },
+      function (err, note) {
+        if (err) {
+          return res.status(500).json({
+            message: "Error deleting a note",
+            error: err,
+          });
+        }
+        return res.status(204).json();
+      }
+    );
   },
 };
--- routes/notebookRoutes.js
+++ routes/notebookRoutes.js
@@ -5,25 +5,24 @@ var notebookController = require("../controllers/notebookController.js");
 /*
  * GET
  */
+router.get("/", notebookController.list); //returns all notebooks
 
 /*
  * POST
  */
 router.post("/", notebookController.create);
-router.post("/:id", notebookController.addNote);
+router.post("/addNote", notebookController.addNote);
 
 /*
  * PUT
  */
 router.put("/:id", notebookController.update);
+router.put("/:notebookId/:noteId", notebookController.updateNote);
 
 /*
  * DELETE
  */
 router.delete("/:id", notebookController.remove);
-router.delete(
-  "/:notebookId/deleteNote/:commentId",
-  notebookController.removeNote
-);
+router.delete("/:notebookId/deleteNote/:noteId", notebookController.removeNote);
 
 module.exports = router;
